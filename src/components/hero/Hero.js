cea51ea546d1a343784d6d18822e91866ade8c5d
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-02-19 16:07:54 -0800

    styling

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -17,26 +17,24 @@ const Hero = () => {
 
 
             <div className="hero-container">
-                <div className="content">
-                    <Fade right>
-                        <h1>33RD STREET RECOVERY</h1>
-                    </Fade>
-                    <Fade left>
-                        <h2 className='subtitle'>Substance Abuse Addiction and <br/>Mental Health Treatment Facility</h2>
-                    </Fade>
-                        <br />
-                        <div className='hero-btn'>
-                        <Link to="/contact">
-                        <button>Get an Appointment</button>
-                    </Link>
-                    </div>
-                    <br />
-                    <Fade bottom>
-                        <p>Walk the road of recovery with us</p>
-                    </Fade>
-                </div>
+        <div className="content">
+                <h1 className="animate-charcter">33RD STREET RECOVERY</h1>
+            <Fade left>
+                <h2 className='subtitle'>Substance Abuse Addiction and Mental Health Treatment Facility</h2>
+            </Fade>
+                <br />
+                <div className='hero-button'>
+                <Link to="/contact">
+                <button>GET AN APPOINTMENT</button>
+            </Link>
             </div>
+            <br />
+            <Fade bottom>
+                <p>Find your happiness and learn to put it first.</p>
+            </Fade>
         </div>
+    </div>
+</div>
 </>
 
     )
