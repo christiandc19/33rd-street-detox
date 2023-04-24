109c282157d4b48e49c6070e24292e9b1ba4f63a
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-04-24 10:20:26 -0700

    styling

--- src/components/Whoweare/Whoweare.js
+++ src/components/Whoweare/Whoweare.js
@@ -39,11 +39,11 @@ const Whoweare = () => {
                 </p>
                 </Fade>
 
-                <Fade left>
+                {/* <Fade left>
                 <p>
                 To ensure that those who attend our rehab facility receive tailored care to meet their individual needs, we conduct a series of evaluations before you begin treatment at our drug and alcohol detox center. <br/> <br/> These evaluations allow us to understand your active addictions, history of substance abuse, mental health disorders, co-occurring disorders, prior drug and alcohol treatment, and how our detox center can help you overcome your substance use disorder.
                 </p>
-                </Fade>
+                </Fade> */}
                  <br />
                 <ul>
                     <Fade left>
