0d69eea06069f249dc683a65c12b380ea99960eb
Christian Carandang <91305708+christiandc19@users.noreply.github.com>
2023-06-09 13:53:45 -0700

    styling

--- src/components/mission/Mission.js
+++ src/components/mission/Mission.js
@@ -4,18 +4,13 @@ import Fade from 'react-reveal/Fade';
 
 import './Mission.css'
 
-import Wave from '../../assets/waves.svg'
-
-
 const Mission = () => {
     return (
 
 <>
 
         <div className='mission main-menu'>
-        <div className='hero-divider'>
-                <img src={Wave} loading="lazy" repeat-x alt='wave divider'/>
-            </div>
+
                 <div className="mission-content">
                     <Fade top>
                     <h1>MISSION</h1>
